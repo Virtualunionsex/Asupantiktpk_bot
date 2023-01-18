8afdf00795c70167113aaced820c7a20ac7f4101
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-01-18 22:47:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5042563853 1374057358 1854578847").split()):
+    for x in (os.environ.get("ADMINS", "1821550386 1374057358 1854578847").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -50,7 +50,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 ADMINS.append(1441775402)
-ADMINS.append(1854578847)
+ADMINS.append(1821550386)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
