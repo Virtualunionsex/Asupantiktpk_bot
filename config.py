668719929d9127c551eaa16b73fbb177fa9c8627
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-04-06 20:17:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5852106103:AAEFt4Ln3NNu5uAI-iJWd34-t6n631Tmg5g")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5646986250:AAH0hz6gY5QtdWhtQ-bxZ5W_AS0yU6IUOcA")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "16246834"))
@@ -12,33 +12,33 @@ APP_ID = int(os.environ.get("APP_ID", "16246834"))
 API_HASH = os.environ.get("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001756470619"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001810517856"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5680450278"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1245451624"))
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "postgres://rwgmydpn:1s_ykVlSf3iZ-eznaY3Ke8I8iDYxdG9d@batyr.db.elephantsql.com/rwgmydpn")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://lbppnpzg:anX6J9dllmK1q0R9hGHUykgqGcTJfw_4@rosie.db.elephantsql.com/lbppnpzg")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1001861704447"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001815711149"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001569500029"))
+FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1001340728711"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001827221239"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001673770607"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\npromo vvip 9 grup total puluhan ribu video dengan harga 50K. Hub @panggilaja_m.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nJOIN VVIP MURAH DAN TERPERCAYA DI @VIPEXOTIS")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1908660708 5680450278 1715348447").split()):
+    for x in (os.environ.get("ADMINS", "5589797950 1245451624 2110216783").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nJOIN VVIP MURAH DAN TERPERCAYA 50K SUDAH DAPAT 9 GRUP/CHANNEL DENGAN TOTAL VIDEO PULUHAN RIBU. HUB @PANGGILAJA_M</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>JOIN VVIP MURAH DAN TERPERCAYA DI @VIPEXOTIS</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -49,8 +49,9 @@ if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
 else:
     DISABLE_CHANNEL_BUTTON = False
 
-ADMINS.append(5680450278)
-ADMINS.append(1908660708)
+ADMINS.append(5589797950)
+ADMINS.append(1245451624)
+ADMINS.append(2110216783) 
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
